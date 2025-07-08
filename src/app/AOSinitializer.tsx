bc3ec18d7913880ae0558b0836a611b1a8a81df3
boenthi <boenthi989@gmil.com>
2025-07-08 15:48:21 +0700

    First release of Hello World!

---
+++ src/app/AOSinitializer.tsx
@@ -0,0 +1,21 @@
+
+"use client";
+
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const AosInitializer = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      mirror: false,
+    });
+  }, []);
+
+  return null;
+};
+
+export default AosInitializer;
+
